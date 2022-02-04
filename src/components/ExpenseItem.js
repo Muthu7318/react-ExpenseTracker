01baf414b5ae4a26b450da11ae75b786d350ef9a
Muthu7318 <muthumarshal@gmail.com>
2022-02-04 15:57:26 +0530

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,20 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 500;
+function ExpenseItem(props) {
+  console.log(props);
+  const month = props.date.toLocaleString("en-us", { month: "long" });
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{`${expenseDate.getDate()}/${
-        expenseDate.getMonth() + 1
-      }/${expenseDate.getFullYear()}`}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{`$${expenseAmount}`}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{`$${props.amount}`}</div>
       </div>
     </div>
   );
