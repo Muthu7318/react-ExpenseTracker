da1c1269b8671f518eb2164f0c7e532683ac1454
Muthu7318 <muthumarshal@gmail.com>
2022-02-04 14:55:53 +0530

    styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <h2>Food</h2>
-      <div>March 28 2022</div>
-      <div>500</div>
+      <div className="expense-item__description">March 28 2022</div>
+      <div className="expense-item__price">500</div>
     </div>
   );
 }
